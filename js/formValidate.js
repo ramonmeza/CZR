75713190798477cadc49cda1959c6191c414a76d
Ramon Meza <ramon_meza@live.com>
2024-07-15 17:20:26 -0400

    Form validation fixes

--- js/formValidate.js
+++ js/formValidate.js
@@ -19,70 +19,94 @@
     document.getElementById('subscribe-btn').addEventListener('click', async function (e) {
         let isValid = validateEmail(emailInput.value);
 
-        if (isValid) {
-            const email = emailInput.value;
-
-            const response = await fetch(
-                API_URL + '/subscribe', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams({
-                    'email': email
-                })
-            });
-
-            console.log(response);
-
-            // display status message to user
-            message.classList.remove('text-danger');
-            message.classList.add('text-success');
-            message.innerHTML = 'Successfully subscribed!';
-            emailInput.classList.add('is-valid');
-            emailInput.classList.remove('is-invalid');
-        } else {
+        if (!isValid) {
             // display status message to user
             message.classList.add('text-danger');
             message.classList.remove('text-success');
             message.innerHTML = 'Failed to subscribe';
             emailInput.classList.add('is-invalid');
             emailInput.classList.remove('is-valid');
+            return;
         }
+
+        const email = emailInput.value;
+
+        const response = await fetch(
+            API_URL + '/subscribe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                'email': email
+            })
+        });
+
+        if (!response.ok) {
+            // display status message to user
+            message.classList.add('text-danger');
+            message.classList.remove('text-success');
+            message.innerHTML = 'Failed to subscribe';
+            emailInput.classList.add('is-invalid');
+            emailInput.classList.remove('is-valid');
+            return;
+        }
+
+        console.log(response.json());
+
+        // display status message to user
+        message.classList.remove('text-danger');
+        message.classList.add('text-success');
+        message.innerHTML = 'Successfully subscribed!';
+        emailInput.classList.add('is-valid');
+        emailInput.classList.remove('is-invalid');
+
     }, false);
 
     document.getElementById('unsubscribe-btn').addEventListener('click', async function (e) {
         let isValid = validateEmail(emailInput.value);
 
-        if (isValid) {
-            const email = emailInput.value;
-
-            const response = await fetch(
-                API_URL + '/subscribe', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams({
-                    'email': email
-                })
-            });
-
-            console.log(response);
-
-            // display status message to user
-            message.classList.remove('text-danger');
-            message.classList.add('text-success');
-            message.innerHTML = 'Successfully unsubscribed';
-            emailInput.classList.add('is-valid');
-            emailInput.classList.remove('is-invalid');
-        } else {
+        if (!isValid) {
             // display status message to user
             message.classList.add('text-danger');
             message.classList.remove('text-success');
             message.innerHTML = 'Failed to unsubscribe. Please contact email listed at the bottom of the <a href="privacy.html">Privacy Policy</a> for assistance.';
             emailInput.classList.add('is-invalid');
             emailInput.classList.remove('is-valid');
+            return;
         }
+
+        const email = emailInput.value;
+
+        const response = await fetch(
+            API_URL + '/subscribe', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                'email': email
+            })
+        });
+
+
+        if (!response.ok) {
+            // display status message to user
+            message.classList.add('text-danger');
+            message.classList.remove('text-success');
+            message.innerHTML = 'Failed to unsubscribe. Please contact email listed at the bottom of the <a href="privacy.html">Privacy Policy</a> for assistance.';
+            emailInput.classList.add('is-invalid');
+            emailInput.classList.remove('is-valid');
+            return;
+        }
+
+        console.log(response.json())
+
+        // display status message to user
+        message.classList.remove('text-danger');
+        message.classList.add('text-success');
+        message.innerHTML = 'Successfully unsubscribed';
+        emailInput.classList.add('is-valid');
+        emailInput.classList.remove('is-invalid');
     }, false);
 })()
