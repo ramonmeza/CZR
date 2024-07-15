2642e329e143e3fe36a334565253deccaabf50d2
Ramon Meza <ramon_meza@live.com>
2024-07-15 17:26:32 -0400

    Consolidate code

--- js/formValidate.js
+++ js/formValidate.js
@@ -3,6 +3,11 @@
 
     const API_URL = 'https://speoovq9o2.execute-api.us-east-1.amazonaws.com';
 
+    const subscribeForm = document.getElementById('subscribe-form');
+    const emailInput = document.getElementById('subscriber-email');
+    const message = document.getElementById('subscribe-message');
+
+
     function validateEmail(email) {
         const matched = String(email)
             .toLowerCase()
@@ -12,11 +17,7 @@
         return Boolean(matched)
     }
 
-    const subscribeForm = document.getElementById('subscribe-form');
-    const emailInput = document.getElementById('subscriber-email');
-    const message = document.getElementById('subscribe-message');
-
-    document.getElementById('subscribe-btn').addEventListener('click', async function (e) {
+    async function doWork(method) {
         let isValid = validateEmail(emailInput.value);
 
         if (!isValid) {
@@ -33,7 +34,7 @@
 
         const response = await fetch(
             API_URL + '/subscribe', {
-            method: 'POST',
+            method: method,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
@@ -61,54 +62,13 @@
         message.innerHTML = 'Successfully subscribed!';
         emailInput.classList.add('is-valid');
         emailInput.classList.remove('is-invalid');
+    }
 
+    document.getElementById('subscribe-btn').addEventListener('click', async function (e) {
+        doWork('POST');
     }, false);
 
     document.getElementById('unsubscribe-btn').addEventListener('click', async function (e) {
-        let isValid = validateEmail(emailInput.value);
-
-        if (!isValid) {
-            // display status message to user
-            message.classList.add('text-danger');
-            message.classList.remove('text-success');
-            message.innerHTML = 'Failed to unsubscribe. Please contact email listed at the bottom of the <a href="privacy.html">Privacy Policy</a> for assistance.';
-            emailInput.classList.add('is-invalid');
-            emailInput.classList.remove('is-valid');
-            return;
-        }
-
-        const email = emailInput.value;
-
-        const response = await fetch(
-            API_URL + '/subscribe', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams({
-                'email': email
-            })
-        });
-
-
-        if (!response.ok) {
-            // display status message to user
-            message.classList.add('text-danger');
-            message.classList.remove('text-success');
-            message.innerHTML = 'Failed to unsubscribe. Please contact email listed at the bottom of the <a href="privacy.html">Privacy Policy</a> for assistance.';
-            emailInput.classList.add('is-invalid');
-            emailInput.classList.remove('is-valid');
-            return;
-        }
-
-        const json = await response.json();
-        console.log(json);
-
-        // display status message to user
-        message.classList.remove('text-danger');
-        message.classList.add('text-success');
-        message.innerHTML = 'Successfully unsubscribed';
-        emailInput.classList.add('is-valid');
-        emailInput.classList.remove('is-invalid');
+        doWork('DELETE');
     }, false);
 })()
