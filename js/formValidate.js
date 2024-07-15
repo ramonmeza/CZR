03f5f736698a3f499f8dd2a9b33dc747d32dfa55
Ramon Meza <ramon_meza@live.com>
2024-07-15 16:42:32 -0400

    Updating api and form validation

--- js/formValidate.js
+++ js/formValidate.js
@@ -1,20 +1,80 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
+    const API_URL = 'https://speoovq9o2.execute-api.us-east-1.amazonaws.com';
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+    function validateEmail(email) {
+        const matched = String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
+        return Boolean(matched)
+    }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
+    const subscribeForm = document.getElementById('subscribe-form');
+    const emailInput = document.getElementById('subscriber-email');
+    const message = document.getElementById('subscribe-message');
+
+    document.getElementById('subscribe-btn').addEventListener('click', async function (e) {
+        let isValid = validateEmail(emailInput.value);
+
+        if (isValid) {
+            const email = emailInput.value;
+
+            const response = await fetch(
+                API_URL + '/subscribe', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    'email': email
+                })
+            });
+
+            console.log(response);
+
+            // display status message to user
+            message.classList.remove('text-danger');
+            message.classList.add('text-success');
+            message.innerHTML = 'Successfully subscribed!';
+        } else {
+            // display status message to user
+            message.classList.add('text-danger');
+            message.classList.remove('text-success');
+            message.innerHTML = 'Failed to subscribe';
+        }
+    }, false);
+
+    document.getElementById('unsubscribe-btn').addEventListener('click', async function (e) {
+        let isValid = validateEmail(emailInput.value);
+
+        if (isValid) {
+            const email = emailInput.value;
+
+            const response = await fetch(
+                API_URL + '/subscribe', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    'email': email
+                })
+            });
+
+            console.log(response);
+
+            // display status message to user
+            message.classList.remove('text-danger');
+            message.classList.add('text-success');
+            message.innerHTML = 'Successfully unsubscribed';
+        } else {
+            // display status message to user
+            message.classList.add('text-danger');
+            message.classList.remove('text-success');
+            message.innerHTML = 'Failed to unsubscribe. Please contact email listed at the bottom of the <a href="privacy.html">Privacy Policy</a> for assistance.';
+        }
+    }, false);
 })()
